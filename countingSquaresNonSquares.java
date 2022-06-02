6cd6750f29eaeac785df389aaaa8b4b254614a75
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-06-02 19:17:24 +0100

    Update countingSquaresNonSquares.java

--- countingSquaresNonSquares.java
+++ countingSquaresNonSquares.java
@@ -1,2 +1,32 @@
 import java.io.*;
 import java.math.*;
+import java.util.*;  
+
+class GFG
+{
+  
+  static void countSquaresNonSquares(int n)
+  {
+    int sc = (int)(Math.floor(Math.sqrt(n)));
+    System.out.println("Number Of Square Numbers: " + sc);
+    System.out.println("Number Of Non-Square Numbers: " + (n - sc))
+  
+  
+  }
+  
+  public static void main(String args[])
+  {
+    
+    new Scanner scTwo = new Scanner(System.in)
+      
+    int a = scTwo.nextInt()
+      
+    n = a
+      
+    countSquaresNonSquares(n)
+  
+  
+  }
+
+
+}
